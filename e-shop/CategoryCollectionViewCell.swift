b2217730d9909897c5e0a93218a2fec5e878cc5d
MYarovoi <m.yarovoi96@gmail.com>
2024-07-26 13:34:24 +0300

    Custom collection view Cell class

---
+++ e-shop/CategoryCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryCollectionViewCell.swift
+//  e-shop
+//
+//  Created by Mykyta Yarovoi on 26.07.2024.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+}

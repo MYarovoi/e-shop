0c0696e0e67bf9f8d13390c5116850343eaaa39e
MYarovoi <m.yarovoi96@gmail.com>
2024-08-13 15:00:03 +0300

    Login view UI

---
+++ e-shop/Main Views/WelcomeViewController.swift
@@ -0,0 +1,45 @@
+//
+//  WelcomeViewController.swift
+//  e-shop
+//
+//  Created by Mykyta Yarovoi on 13.08.2024.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var resendButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+    @IBAction func registerButtonPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+    @IBAction func forgotPasswordButtonPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+    @IBAction func resendEmailButtonPressed(_ sender: UIButton) {
+        
+        
+    }
+}

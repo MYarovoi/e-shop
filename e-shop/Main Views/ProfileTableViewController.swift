edb0f569f264109c18ca8359a319e3f06c365a66
MYarovoi <m.yarovoi96@gmail.com>
2024-08-15 15:21:32 +0300

    Checking onboarding status

--- e-shop/Main Views/ProfileTableViewController.swift
+++ e-shop/Main Views/ProfileTableViewController.swift
@@ -11,6 +11,7 @@ class ProfileTableViewController: UITableViewController {
     
     @IBOutlet weak var finishRegistrationButton: UIButton!
     @IBOutlet weak var purchaseHistoryButton: UIButton!
+    var editBarButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,24 +22,73 @@ class ProfileTableViewController: UITableViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
+        checkLoginStatus()
+        checkOnBoardingStatus()
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return 3
     }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-        return UITableViewCell()
+    
+    private func checkOnBoardingStatus() {
+        
+        if MUser.currentUser() != nil {
+            
+            if MUser.currentUser()!.onBoard {
+                
+                finishRegistrationButton.setTitle("Account is active", for: .normal)
+                finishRegistrationButton.isEnabled = false
+            } else {
+                
+                finishRegistrationButton.setTitle("Finish registration", for: .normal)
+                finishRegistrationButton.isEnabled = true
+                finishRegistrationButton.tintColor = .red
+            }
+        } else {
+            finishRegistrationButton.setTitle("Logged out", for: .normal)
+            finishRegistrationButton.isEnabled = false
+            purchaseHistoryButton.isEnabled = false
+        }
     }
     
-    // MARK: - Navigation
-
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-
+    private func checkLoginStatus() {
+        
+        if MUser.currentUser() == nil {
+            
+            createBarButtonWith(title: "Login")
+        } else {
+            createBarButtonWith(title: "Edit")
+        }
+    }
+    
+    private func createBarButtonWith(title: String) {
+        
+        editBarButton = UIBarButtonItem(title: title, style: .plain, target: self, action: #selector(rightBarButtonPressed))
+        self.navigationItem.rightBarButtonItem = editBarButton
+    }
+    
+    @objc func rightBarButtonPressed() {
+        
+        if editBarButton.title == "Login" {
+            
+            showLoginView()
+        } else {
+            
+            goToEditProfile()
+        }
+    }
+    
+    private func showLoginView() {
+        
+        let loginView = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "loginView")
+        self.present(loginView, animated: true)
+    }
+    
+    private func goToEditProfile() {
+        
+        
     }
 }

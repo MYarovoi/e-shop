62376a1001411ea638211f380faab41e65c65d7f
MYarovoi <m.yarovoi96@gmail.com>
2024-08-19 17:52:10 +0300

    Show/Hide Search view

---
+++ e-shop/Main Views/SearchViewController.swift
@@ -0,0 +1,94 @@
+//
+//  SearchViewController.swift
+//  e-shop
+//
+//  Created by Mykyta Yarovoi on 19.08.2024.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var searchOptionsView: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchButtonOutlet: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        searchTextField.addTarget(self, action: #selector(self.textFieldDidChanged(_:)), for: UIControl.Event.editingChanged)
+        
+        tableView.tableFooterView = UIView()
+    }
+    
+    @IBAction func searchBarButtonPressed(_ sender: UIBarButtonItem) {
+        
+        dismissKeyboard()
+        showSearchField()
+    }
+    
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+    //MARK: - Helpers
+    
+    private func emptyTextField() {
+        
+        searchTextField.text = ""
+    }
+    
+    private func dismissKeyboard() {
+        
+        self.view.endEditing(false)
+    }
+    
+    @objc func textFieldDidChanged (_ textField: UITextField) {
+        
+        searchButtonOutlet.isEnabled = textField.text != ""
+        
+        if searchButtonOutlet.isEnabled {
+            
+            searchButtonOutlet
+        } else {
+            
+            disableSearchButton()
+        }
+    }
+    
+    private func disableSearchButton() {
+        
+        searchButtonOutlet.isEnabled = false
+    }
+    
+    private func showSearchField() {
+        
+        disableSearchButton()
+        emptyTextField()
+        animateSearchOptionIn()
+    }
+    
+    private func animateSearchOptionIn() {
+        
+        UIView.animate(withDuration: 0.5) {
+            
+            self.searchOptionsView.isHidden = !self.searchOptionsView.isHidden
+        }
+    }
+}
+
+extension SearchViewController: UITabBarDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
+    
+}
